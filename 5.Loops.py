653601e0fd56921a80f11ec04c6090dabd646b26
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-07-05 04:18:59 +0530

    #devsnest150daychallenge

---
+++ 5.Loops.py
@@ -0,0 +1,16 @@
+# Task
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+
+# Example
+
+# The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+
+# 0
+# 1
+# 4
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        s=i**2
+        print(s)
