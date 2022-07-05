1f9b899f77fdd2a3b02b2b6a2866ec1feeac560c
shubhamsigdar1 <shubhamsigdar@gmail.com>
2022-07-05 18:11:48 +0530

    #devsnest150daychallenge

--- 10.Nested Lists.py
+++ 10.Nested Lists.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     
 
     if __name__ == '__main__':
-    students = []
+    # students = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
